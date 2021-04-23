a61b929de9b012eb5b0bdd37ceb8348ffbe1da01
Marcus Behel <marcus.behel@mindspring.com>
2021-04-23 00:35:10 -0400

    Working on UI design

--- src/DriveStation.cpp
+++ src/DriveStation.cpp
@@ -1,6 +1,17 @@
 
 #include <DriveStation.h>
 
+#include <QFile>
+#include <QPalette>
+
 DriveStation::DriveStation(QWidget *parent){
+    
     ui.setupUi(this);
+
+    // Append version to window title
+    QFile versionFile(":/version.txt");
+    if(versionFile.open(QIODevice::ReadOnly)){
+        QString ver = versionFile.readLine().replace("\n", "").replace("\r", "");
+        setWindowTitle(windowTitle() + " v" + ver);
+    }
 }
