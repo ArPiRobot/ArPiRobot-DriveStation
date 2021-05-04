ac258175bd5dde9941f9aa422dacbd909c8191d8
Marcus Behel <marcus.behel@mindspring.com>
2021-05-04 01:24:50 -0400

    Update gitignore

--- launcher.py
+++ launcher.py
@@ -1,6 +1,6 @@
 """
 This script is simply used to load the actual program. This allows using pyinstaller, cx_Freeze, or
-other similar tools to make this script into a platform specific binary. Shis method allows updating
+other similar tools to make this script into a platform specific binary. This method allows updating
 the real application's python code without needing to rebuild the platform specific binary. This is
 useful because there is no way to cross build for other platforms using the tools mentioned before,
 nor is there an easy way to embed a python interpeter in an application package.
