abd4eb82aeafda25eb1779015f57e520773666fa
Marcus Behel <marcus.behel@mindspring.com>
2021-06-27 01:09:47 -0400

    Added util class to load and change themes

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from PySide6.QtWidgets import QApplication, QStyleFactory
 from PySide6.QtCore import Qt, QFile, QIODevice
 
 from drive_station import DriveStationWindow
-
+from src.util import ThemeManager
 
 QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
 QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
@@ -19,26 +19,11 @@ except AttributeError:
 
 app = QApplication(sys.argv)
 
-# Custom stylesheet used in UI files designed for fusion base
+# Custom stylesheet used is designed for fusion base
 app.setStyle(QStyleFactory.create("Fusion"))
 
-# Load stylesheet. This is a stylesheet with placeholders. It cannot be used directly
-stylesheet_str = ""
-stylesheet_file = QFile(":/stylesheet.qss")
-if stylesheet_file.open(QIODevice.ReadOnly):
-    stylesheet_str = bytes(stylesheet_file.readAll()).decode()
-stylesheet_file.close()
-
-# TODO: Generate different stylesheets for each variable set to allow different color schemes
-# Substitute values for placeholders in stylesheet
-dark_file = QFile(":/stylesheet-vars/dark.csv")
-if dark_file.open(QIODevice.ReadOnly):
-    for line in bytes(dark_file.readAll()).decode().splitlines(False):
-        # Index 0 = variable, Index 1 = value
-        parts = line.replace(", ", ",").split(",")
-        stylesheet_str = stylesheet_str.replace(f"@{parts[0]}@", parts[1])
-
-app.setStyleSheet(stylesheet_str)
+ThemeManager.load_themes()
+ThemeManager.apply_theme(app, "Dark")
 
 ds = DriveStationWindow()
 ds.show()
--- src/util.py
+++ src/util.py
@@ -1,4 +1,6 @@
-from PySide6.QtCore import QSize
+from typing import List
+
+from PySide6.QtCore import QSize, QFile, QIODevice, QDirIterator, QFileInfo
 from PySide6.QtGui import QTextDocument, QAbstractTextDocumentLayout
 from PySide6.QtWidgets import QStyledItemDelegate, QStyleOptionViewItem, QApplication, QStyle
 
@@ -43,3 +45,54 @@ class HTMLDelegate(QStyledItemDelegate):
 
     def sizeHint(self, option, index):
         return QSize(self.doc.idealWidth(), self.doc.size().height())
+
+
+class ThemeManager:
+    __BASE_STYLESHEET = ":/stylesheet.qss"
+    __THEME_PATH = ":/stylesheet-vars/"
+    __themes: List[str] = []
+
+    @staticmethod
+    def load_themes():
+        # Load a list of theme names, but do not generate stylesheets yet.
+        # Pre-generating multiple stylesheets wastes memory
+        iterator = QDirIterator(ThemeManager.__THEME_PATH)
+        while iterator.hasNext():
+            info = QFileInfo(iterator.next())
+            if info.completeSuffix().lower() == "csv":
+                ThemeManager.__themes.append(info.baseName())
+
+    @staticmethod
+    def themes() -> List[str]:
+        return ThemeManager.__themes.copy()
+
+    @staticmethod
+    def apply_theme(app: QApplication, theme: str) -> bool:
+        if theme not in ThemeManager.__themes:
+            print("A")
+            return False
+
+        # Load stylesheet. This is a stylesheet with placeholders. It cannot be used directly
+        stylesheet_str = ""
+        stylesheet_file = QFile(":/stylesheet.qss")
+        if stylesheet_file.open(QIODevice.ReadOnly):
+            stylesheet_str = bytes(stylesheet_file.readAll()).decode()
+        else:
+            return False
+        stylesheet_file.close()
+
+        # Substitute values for placeholders in stylesheet
+        dark_file = QFile(f"{ThemeManager.__THEME_PATH}/{theme}.csv")
+        if dark_file.open(QIODevice.ReadOnly):
+            for line in bytes(dark_file.readAll()).decode().splitlines(False):
+                # Index 0 = variable, Index 1 = value
+                parts = line.replace(", ", ",").split(",")
+                stylesheet_str = stylesheet_str.replace(f"@{parts[0]}@", parts[1])
+        else:
+            return False
+
+        app.setStyleSheet(stylesheet_str)
+
+        return True
+
+
