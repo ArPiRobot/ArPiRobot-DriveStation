c7e5992b29cb86df3125edc7acdfb6d8df26ec71
Marcus Behel <marcus.behel@mindspring.com>
2021-06-27 00:52:27 -0400

    Made stylesheet use placeholders to allow for multiple color schemes

--- src/main.py
+++ src/main.py
@@ -22,10 +22,23 @@ app = QApplication(sys.argv)
 # Custom stylesheet used in UI files designed for fusion base
 app.setStyle(QStyleFactory.create("Fusion"))
 
+# Load stylesheet. This is a stylesheet with placeholders. It cannot be used directly
+stylesheet_str = ""
 stylesheet_file = QFile(":/stylesheet.qss")
 if stylesheet_file.open(QIODevice.ReadOnly):
-    app.setStyleSheet(bytes(stylesheet_file.readAll()).decode())
+    stylesheet_str = bytes(stylesheet_file.readAll()).decode()
 stylesheet_file.close()
+
+# TODO: Generate different stylesheets for each variable set to allow different color schemes
+# Substitute values for placeholders in stylesheet
+dark_file = QFile(":/stylesheet-vars/dark.csv")
+if dark_file.open(QIODevice.ReadOnly):
+    for line in bytes(dark_file.readAll()).decode().splitlines(False):
+        # Index 0 = variable, Index 1 = value
+        parts = line.replace(", ", ",").split(",")
+        stylesheet_str = stylesheet_str.replace(f"@{parts[0]}@", parts[1])
+
+app.setStyleSheet(stylesheet_str)
 
 ds = DriveStationWindow()
 ds.show()
