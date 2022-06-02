5f5a16403cefc26e3f057aa173b5a9ee3ea43a50
Marcus Behel <marcus.behel@mindspring.com>
2022-06-02 17:58:42 -0400

    Fix change version script

--- change-version.py
+++ change-version.py
@@ -9,6 +9,9 @@ def replaceInFileRegex(filename, a, b):
     with open(filename, 'r') as file :
         filedata = file.read()
 
+    # Make sure things still work on windows
+    filedata.replace("\r\n", "\n")
+
     # Replace the target string
     filedata = re.sub(a, b, filedata, re.MULTILINE)
 
@@ -20,18 +23,11 @@ if len(sys.argv) != 2:
     print("Usage: " + sys.argv[0] + " VERSION_NAME")
     exit(1)
 
-# Change in resource used by java app
+# Change in resource used by python app
 with open("res/version.txt", 'w') as file:
         file.write(sys.argv[1])
 
+# Change in package resources
 replaceInFileRegex("packaging/windows/win_installer.iss", "#define MyAppVersion \".*\"", "#define MyAppVersion \"" + sys.argv[1] + "\"")
-
-# replaceInFileRegex("macos_resources/Info.plist", "<key>CFBundleShortVersionString</key>\n\s<string>.*</string>", "<key>CFBundleShortVersionString</key>\n\t<string>" + sys.argv[1] + "</string>")
-# replaceInFileRegex("macos_resources/Info.plist", "<key>CFBundleVersion</key>\n\s<string>.*</string>", "<key>CFBundleVersion</key>\n\t<string>" + sys.argv[1] + "</string>")
-
-replaceInFileRegex("packaging/linux_pyinstaller/deb_control", "Version: .*\n", "Version: " + sys.argv[1] + "\n")
 replaceInFileRegex("packaging/linux_source/deb_control", "Version: .*\n", "Version: " + sys.argv[1] + "\n")
-
-
-# replaceInFileRegex("linux_resources/PKGBUILD", "pkgver=.*\n", "pkgver=" + sys.argv[1] + "\n")
-# replaceInFileRegex("linux_resources/PKGBUILD", "pkgrel=.*\n", "pkgrel=" + sys.argv[1].replace(".", "") + "\n")
+replaceInFileRegex("packaging/linux_appimage/arpirobot-drivestation.desktop", "X-AppImage-Version=.*\n", "X-AppImage-Version={0}\n".format(sys.argv[1]))
