2f029ee9a78e657c031172e4c33c4719a544c790
Marcus Behel <marcus.behel@mindspring.com>
2023-12-06 20:04:50 -0500

    Use qt 6.6.1 and remove dark mode issue workarrounds

--- src/main.py
+++ src/main.py
@@ -45,15 +45,9 @@ if platform.system() == "Linux":
     plat_name = return_dict['plat_name']
     if plat_name == "wayland" and os.environ['XDG_CURRENT_DESKTOP'].find("GNOME") != -1:
         # Running with wayland platform plugin in a gnome session
-        cursor_size = int(subprocess.check_output(["gsettings", "get", "org.gnome.desktop.interface", "cursor-size"]))
         text_scale_factor = float(subprocess.check_output(["gsettings", "get", "org.gnome.desktop.interface", "text-scaling-factor"]))
-        cursor_theme = subprocess.check_output(["gsettings", "get", "org.gnome.desktop.interface", "cursor-theme"]).decode()
         if 'QT_FONT_DPI' not in os.environ:
             os.environ['QT_FONT_DPI'] = str(int(text_scale_factor * 96))
-        if 'XCURSOR_SIZE' not in os.environ:
-            os.environ['XCURSOR_SIZE'] = str(cursor_size)
-        if 'XCURSOR_THEME' not in os.environ:
-            os.environ['XCURSOR_THEME'] = cursor_theme[1:-2]
 
 app = QApplication(sys.argv)
 app.setStyle("Fusion")
@@ -68,25 +62,6 @@ if app.styleHints().colorScheme() == Qt.ColorScheme.Dark:
         for cg in [QPalette.ColorGroup.Active, QPalette.ColorGroup.Current, QPalette.ColorGroup.Disabled, QPalette.ColorGroup.Inactive]:
             p.setColor(cg, QPalette.ColorRole.Base, p.color(cg, QPalette.ColorRole.Base))
         app.setPalette(p)
-    elif platform.system() == "Darwin":
-        # TODO: Button text color wrong when inactive
-        pass
-    else:
-        is_plasma = os.environ['XDG_CURRENT_DESKTOP'].find("KDE") != -1
-        is_lxqt = os.environ['XDG_CURRENT_DESKTOP'].find("LXQt") != -1
-
-        if not is_lxqt and not is_plasma:
-            # Some disabled and inactive colors are wrong on gtk desktops
-            # This does not impact plasma (and presumably would not impact LXQt)
-            # https://bugreports.qt.io/browse/QTBUG-113486
-            p = app.palette()
-            p.setColor(QPalette.ColorGroup.Inactive, QPalette.ColorRole.Button, p.color(QPalette.ColorGroup.Active, QPalette.ColorRole.Button))
-            p.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole.Button, p.color(QPalette.ColorGroup.Active, QPalette.ColorRole.Button).lighter())
-            # TODO: Button borders
-            app.setPalette(p)
-
-            # TODO: Sometimes (manjaro but not ubuntu), inactive text color (buttontext) is wrong like macos
-            pass
 
 ds = DriveStationWindow()
 
