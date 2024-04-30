601be1acc4bd02020f68f07415855594ee51c846
Marcus Behel <marcus.behel@mindspring.com>
2024-04-29 22:29:52 -0400

    More linux package fixes

--- packaging/linux.sh
+++ packaging/linux.sh
@@ -47,7 +47,7 @@ cp io.github.arpirobot.DriveStation.desktop dist/ArPiRobot-DriveStation
 cp io.github.arpirobot.DriveStation.appdata.xml dist/ArPiRobot-DriveStation
 cp ../../res/icon.png dist/ArPiRobot-DriveStation
 mv dist/ArPiRobot-DriveStation/ArPiRobot-DriveStation dist/ArPiRobot-DriveStation/AppRun
-mkdir ./dist/
+mkdir ../dist/
 ../appimagetool dist/ArPiRobot-DriveStation ../dist/ArPiRobot-DriveStation-Liunx-x64.AppImage
 popd > /dev/null
 
